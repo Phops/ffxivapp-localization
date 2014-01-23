f7c39314f6061f0c1839fafee440ff430c5c2222
Ryan Wilson <syndicated.life@gmail.com>
2014-01-22 22:00:09 -0800

    initial commit

---
+++ FFXIVAPP.Localization/French/Locale.Client.cs
@@ -0,0 +1,98 @@
+﻿// FFXIVAPP.Localization
+// Locale.Client.cs
+// 
+// Copyright © 2013 ZAM Network LLC
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+// following conditions are met: 
+// 
+//  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
+//    disclaimer. 
+//  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+//    following disclaimer in the documentation and/or other materials provided with the distribution. 
+//  * Neither the name of ZAM Network LLC nor the names of its contributors may be used to endorse or promote products 
+//    derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+namespace FFXIVAPP.Localization.French
+{
+    internal static partial class Locale
+    {
+        private static void SetClientLocale()
+        {
+            Dictionary.Add("app_AllowPluginCommandsHeader", "Autoriser les commandes de plugin");
+            Dictionary.Add("app_AttachProcessButtonText", "Lier le processus sélectionné");
+            Dictionary.Add("app_ChangeThemeHeader", "Changer le thème");
+            Dictionary.Add("app_CharacterInformationHeader", "Informations du personnage");
+            Dictionary.Add("app_CharacterSettingsTabHeader", "Paramètres du personnage");
+            Dictionary.Add("app_CodeHeader", "Code");
+            Dictionary.Add("app_CodeLabel", "Code:");
+            Dictionary.Add("app_ColorHeader", "Couleur");
+            Dictionary.Add("app_ColorLabel", "Couleur:");
+            Dictionary.Add("app_ColorSettingsTabHeader", "Paramètres des couleurs");
+            Dictionary.Add("app_ComingSoonText", "A venir!");
+            Dictionary.Add("app_CopyrightLabel", "Droits d'auteur");
+            Dictionary.Add("app_CurrentLabel", "Actuelle:");
+            Dictionary.Add("app_DefaultSettingsButtonText", "Paramètres par défaut");
+            Dictionary.Add("app_DeleteMessage", "Supprimer");
+            Dictionary.Add("app_DescriptionHeader", "Description");
+            Dictionary.Add("app_DescriptionLabel", "Description:");
+            Dictionary.Add("app_EnableNLogHeader", "Activer le logging avec NLog");
+            Dictionary.Add("app_FirstNameLabel", "Prénom:");
+            Dictionary.Add("app_GameLanguageLabel", "Langage du jeu:");
+            Dictionary.Add("app_ImportLodestoneIDButtonText", "Importer l'ID Lodestone");
+            Dictionary.Add("app_InformationMessage", "Information!");
+            Dictionary.Add("app_LastNameLabel", "Nom:");
+            Dictionary.Add("app_LatestLabel", "Dernière:");
+            Dictionary.Add("app_LodestoneIDLabel", "ID Lodestone");
+            Dictionary.Add("app_MainToolTip", "Général");
+            Dictionary.Add("app_MainSettingsTabHeader", "Paramètres généraux");
+            Dictionary.Add("app_CancelButtonText", "Cancel");
+            Dictionary.Add("app_PluginsToolTip", "Plugins");
+            Dictionary.Add("app_PluginSettingsTabHeader", "Paramètres de plugin");
+            Dictionary.Add("app_PluginWarningText", "Ceci permettra à tous les plugins chargés d'envoyer des commandes au jeu. Activer-le si vous y faites confiance.");
+            Dictionary.Add("app_ProcessIDHeader", "ID du processus actuel");
+            Dictionary.Add("app_RefreshProcessButtonText", "Rafraîchir la liste des processus");
+            Dictionary.Add("app_SaveCharacterButtonText", "Sauvegarder le personnage");
+            Dictionary.Add("app_SaveAndClearHistoryToolTip", "Exporter l'historique du chat");
+            Dictionary.Add("app_SaveLogHeader", "Sauvegarder le log");
+            Dictionary.Add("app_ScreenShotToolTip", "Capture d'écran");
+            Dictionary.Add("app_ServerLabel", "Serveur:");
+            Dictionary.Add("app_SettingsToolTip", "Paramètres");
+            Dictionary.Add("app_TabSettingsTabHeader", "Paramètres des onglets");
+            Dictionary.Add("app_UpdateColorButtonText", "Mettre à jour la couleur");
+            Dictionary.Add("app_VersionHeader", "Information des versions");
+            Dictionary.Add("app_VersionLabel", "Version:");
+            Dictionary.Add("app_WarningMessage", "Avertissement!");
+            Dictionary.Add("app_YesButtonText", "Oui");
+            Dictionary.Add("app_OtherOptionsTabHeader", "Autres options");
+            Dictionary.Add("app_AboutToolTip", "À propos");
+            Dictionary.Add("app_ManualUpdateButtonText", "Mise à jour manuelle");
+            Dictionary.Add("app_TranslationsHeader", "Traductions");
+            Dictionary.Add("app_DonationsContributionsHeader", "Dons et Contributions");
+            Dictionary.Add("app_SpecialThanksHeader", "Remerciements spéciaux");
+            Dictionary.Add("app_DownloadNoticeHeader", "Mise à jour possible!");
+            Dictionary.Add("app_DownloadNoticeMessage", "Télécharger?");
+            Dictionary.Add("app_IntegrationWarningText", "Enabling this option means no personally identifable information (game or real life) is sent to the server.  You would be authorization the collection of game related data only.\n\nThe information processed is monster deaths, loot, monster spawn locations, npc and gathering locations.\n\nThis is completely optional and can be turned on or off at any time.");
+            Dictionary.Add("app_EnableHelpLabelsHeader", "Activer Aide étiquettes");
+            Dictionary.Add("app_OKButtonText", "OK");
+            Dictionary.Add("app_TopMostHeader", "Always On Top");
+            Dictionary.Add("app_OfficialPluginsTabHeader", "Official Plugins");
+            Dictionary.Add("app_ThirdPartyPluginsTabHeader", "Third Party Plugins");
+            Dictionary.Add("app_IntegrationSettingsTabHeader", "Integration Settings");
+            Dictionary.Add("app_NoPluginsLineOneTextBlock", "You might have recently turned on or off all plugins; or just have nothing loaded at all.");
+            Dictionary.Add("app_NoPluginsLineTwoTextBlock", "Confirm your settings and if loaded choose a plugin icon from the tab menu.");
+            Dictionary.Add("app_AlwaysReadUpdatesMessage", "Toujours lire l'histoire de la mise à jour de tous les changements.");
+            Dictionary.Add("app_UpdateNotesHeader", "Update Notes");
+            Dictionary.Add("app_ChangesOnRestartMessage", "*Changes will take place after restarting the application.");
+        }
+    }
+}
---
+++ FFXIVAPP.Localization/Japanese/Locale.Informer.cs
@@ -0,0 +1,54 @@
+﻿// FFXIVAPP.Localization
+// Locale.Informer.cs
+// 
+// Copyright © 2013 ZAM Network LLC
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+// following conditions are met: 
+// 
+//  * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
+//    disclaimer. 
+//  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+//    following disclaimer in the documentation and/or other materials provided with the distribution. 
+//  * Neither the name of ZAM Network LLC nor the names of its contributors may be used to endorse or promote products 
+//    derived from this software without specific prior written permission. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+
+namespace FFXIVAPP.Localization.Japanese
+{
+    internal static partial class Locale
+    {
+        private static void SetInformerPluginLocale()
+        {
+            Dictionary.Add("informer_PlayerTabHeader", "プレイヤー");
+            Dictionary.Add("informer_AgroTabHeader", "敵対");
+            Dictionary.Add("informer_TargetsTabHeader", "ターゲット");
+            Dictionary.Add("informer_TargetEnmityTabHeader", "敵対グループ");
+            Dictionary.Add("informer_CurrentPlayerHeader", "現在のプレイヤー");
+            Dictionary.Add("informer_CurrentAgroHeader", "現在の敵対対象");
+            Dictionary.Add("informer_CurrentTargetHeader", "現在のターゲット");
+            Dictionary.Add("informer_FocusTargetHeader", "フォーカスターゲット");
+            Dictionary.Add("informer_MouseOverTargetHeader", "マウスオーバーターゲット");
+            Dictionary.Add("informer_PreviousTargetHeader", "前のターゲット");
+            Dictionary.Add("informer_NameHeader", "名前");
+            Dictionary.Add("informer_LevelLabel", "レベル:");
+            Dictionary.Add("informer_HPLabel", "HP:");
+            Dictionary.Add("informer_MPLabel", "MP:");
+            Dictionary.Add("informer_TPLabel", "TP:");
+            Dictionary.Add("informer_GPLabel", "GP:");
+            Dictionary.Add("informer_CPLabel", "CP:");
+            Dictionary.Add("informer_StatusCountLabel", "ステータスカウント:");
+            Dictionary.Add("informer_EnmityHeader", "敵対グループ:");
+            Dictionary.Add("informer_NameLabel", "名前:");
+            Dictionary.Add("informer_EnmityPercentHeader", "敵対%");
+        }
+    }
+}
